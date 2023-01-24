685586acf3d9938959c4c4a30280846a1f16bb3f
Niaz264 <82967650+Niaz264@users.noreply.github.com>
2023-01-24 22:58:44 +0600

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/codewithweeb/WeebZone"
+    UPSTREAM_REPO = "https://github.com/Niaz264/Abella"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email codewithweeb@gmail.com \
-                 && git config --global user.name Karan \
+                 && git config --global user.email niaznafis265@gmail.com \
+                 && git config --global user.name Niaz264 \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
